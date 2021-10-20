89011ebe3c5f29ef4326db78a82781c27ac166b3
nimrab <nimrab.0@gmail.com>
2021-10-20 23:03:30 +0300

    Routes Nav

--- src/App.tsx
+++ src/App.tsx
@@ -4,26 +4,28 @@ import {Header} from './components/Header/Header';
 import {Navbar} from './components/Navbar/Navbar';
 import {Profile} from './components/Profile/Profile';
 import {Dialogs} from "./components/Dialogs/Dialogs";
-import {Route} from "react-router-dom"
+import {BrowserRouter, Route} from "react-router-dom";
+
 
 
 const App = () => {
 
     return (
-        <div className="main_section">
-            <Header/>
-            <Navbar/>
-            <div className="content">
+        <BrowserRouter>
+            <div className="main_section">
+                <Header/>
+                <Navbar/>
+                <div className="content">
 
-                <Route component={Profile}/>
-                <Route component={Dialogs}/>
+                    <Route path='/profile' component={Profile}/>
+                    <Route path='/dialogs' component={Dialogs}/>
 
-                {/*<Dialogs></Dialogs>*/}
-                {/*<Profile/>*/}
+                    {/*<Dialogs></Dialogs>*/}
+                    {/*<Profile/>*/}
 
+                </div>
             </div>
-        </div>
-
+        </BrowserRouter>
     )
 }
 
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import css from './Navbar.module.css';
+import { NavLink } from 'react-router-dom';
 
 
 
@@ -7,8 +8,8 @@ export const Navbar = () => {
 
     return (
         <div className={css.navbar}>
-            <div className={css.nav_button}><a href="/profile">Profile</a></div>
-            <div className={css.nav_button}><a href="/dialogs">Messages</a></div>
+            <div className={css.nav_button}><NavLink to="/profile" activeClassName={css.active}>Profile</NavLink></div>
+            <div className={css.nav_button}><NavLink to="/dialogs" activeClassName={css.active}>Messages</NavLink></div>
             <div className={css.nav_button}>News</div>
             <div className={css.nav_button}>Music</div>
             <div className={css.nav_button}>Settings</div>
